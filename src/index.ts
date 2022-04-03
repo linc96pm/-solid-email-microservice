67bb8b8847de7f055126dfd5597b435319c2e261
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2022-04-03 21:57:42 +0500

    [app]: finish implemenation

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { NodemailerEmailService } from "./services";
+import { Mail } from "./models/Mail";
+import { EmailAddress } from "./models/EmailAddress";
+import { SendGridEmailService } from "./services/sendgrid/SendGridEmailService";
+import sgMail from '@sendgrid/mail'
+
+async function main () {
+  try {
+    // const nodeMailerTransporter = await NodemailerEmailService.createTestTransporter();
+    // const mailer = new NodemailerEmailService(nodeMailerTransporter);
+    const mailer = new SendGridEmailService('sdfdjhfkjsdhfksdhkfjshdkjf', sgMail)
+    console.log("Started");
+
+    const sourceAddressOrError = EmailAddress.create('khalilstemmler@gmail.com');
+    const destinationAddressOrError = EmailAddress.create('metroidman12@gmail.com');
+    const sourceAddress = sourceAddressOrError.getValue();
+    const destinationAddress = destinationAddressOrError.getValue();
+
+    const mailOrError = Mail.create({ 
+      sourceAddress,
+      destinationAddress,
+      messageTitle: 'Hello world!',
+      messageBody: 'This is an email Im sending.'
+    });
+
+    const mail = mailOrError.getValue();
+
+    const result = await mailer.sendMail(mail);
+    console.log(result);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
---
+++ src/services/sendgrid/SendGridEmailService.ts
@@ -0,0 +1,28 @@
+import { IMailService, IMailTransmissionResult } from "../../models/MailService";
+import { Mail } from "../../models/Mail";
+
+export class SendGridEmailService implements IMailService {
+  private sendgrid: any;
+
+  constructor (apiKey: string, sgInstance: any) {
+    this.sendgrid = sgInstance
+    this.sendgrid.setApiKey(apiKey);
+  }
+
+  async sendMail (email: Mail): Promise<IMailTransmissionResult> {
+    const msg = {
+      to: email.destinationAddress.email,
+      from: email.sourceAddress.email,
+      subject: email.messageTitle,
+      text: email.messageBody,
+      // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    
+    try {
+      await this.sendgrid.send(msg);
+      return { message: 'Success', success: true }
+    } catch (err) {
+      return { message: err.toString(), success: false }
+    }
+  }
+}
